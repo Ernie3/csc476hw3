8eefcc4c36a56024c2fdf297836e36911a7dac07
Ernie3 <ernestgwilsoniii@gmail.com>
2020-10-19 16:39:04 -0400

    got slingshot to launch projectiles, working on clouds

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public static GameObject POI; // point of interest
+
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
+    [Header("Set Dynamically")]
+    public float camZ;
+
+    void Awake()
+    {
+        camZ = transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        if(POI == null)
+            return;
+
+        var destination = POI.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        // interpolate from the current camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.z = camZ;
+        transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
+    }
+}

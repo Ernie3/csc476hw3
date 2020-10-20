e0c2c97ff8ce214992af6f70e15072cb86a09585
Ernie3 <ernestgwilsoniii@gmail.com>
2020-10-19 21:33:18 -0400

    add a bunch of castle prefabs

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public static bool goalMet = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Projectile"))
+        {
+            goalMet = true;
+            var mat = GetComponent<Renderer>().material;
+            var c = mat.color;
+            c.a = 1; // set alpha channel of goal equal to 1
+            mat.color = c;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
